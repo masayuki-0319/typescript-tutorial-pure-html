ea6de78a0fe4e1c59b83c4b80f1e9b6a41abedfb
将之大西 <omccello@gmail.com>
2021-07-06 01:13:49 +0900

    step154

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,25 @@
 import { Configuration } from 'webpack';
-import path from 'path';
+const path = require('path');
 
 const config: Configuration = {
+  mode: 'development',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 };
 
