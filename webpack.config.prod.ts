045ea9c87c3fb6b2e6bdf5013e04866b43a97da3
将之大西 <omccello@gmail.com>
2021-07-06 22:23:28 +0900

    step157

---
+++ webpack.config.prod.ts
@@ -0,0 +1,29 @@
+import { Configuration } from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const path = require('path');
+
+const config: Configuration = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+export default config;
